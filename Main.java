cc8163db4c71a9ef0e717089733f1f1b45ceafc2
Mulik Shrikant Dattatray <120763262+MulikShrikantDattatray14@users.noreply.github.com>
2023-01-05 08:37:47 +0000

    added a basic java code

---
+++ Main.java
@@ -0,0 +1,15 @@
+ 
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello 40");
+      System.out.print("50 World !");
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        
+    }
+}
